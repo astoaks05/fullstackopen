1d9d1dd8263f2fc039e0d21d809db123bbf3e534
Aidan Stoaks <aidanstoaks05@gmail.com>
2025-10-14 15:37:05 -0700

    Part 1 component helper functions destructuring

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,4 @@
+/*
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -66,6 +67,35 @@ const Total = (props) => {
     </div>
   )
 }
+*/
 
+const Hello = ({name, age}) => {
+
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>
+        So you were probably born in {bornYear()}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
 
 export default App
