908f76cdaaf56a625674c4714092e4a36dbe6244
Aidan Stoaks <aidanstoaks05@gmail.com>
2025-12-02 19:06:24 -0800

    Exercise 1.6 complete

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,99 +1,39 @@
-/*
+import { useState } from "react";
+
+const Button = ({onClick, text}) => {
+  return (
+    <button
+    onClick={onClick}
+    >{text}
+    </button>
+  )
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
   }
-
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+  const handleBad = () => {
+    setBad(bad + 1);
+  }
+  
   return (
     <div>
-      <Header course = {course} />  
-      <Content course = {course} /> 
-      <Total course = {course} /> 
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>
-        {props.course.name}
-      </h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part = {props.course.parts[0]}/>
-      <Part part = {props.course.parts[1]}/>
-      <Part part = {props.course.parts[2]}/>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>  
-  )
-}
-
-
-const Total = (props) => {
-  console.log(props)
-  const totalExercises = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
-  return (
-    <div>
-      <p>Number of exercises 
-        : {totalExercises}
-      </p>
-    </div>
-  )
-}
-*/
-
-const Hello = ({name, age}) => {
-
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>
-        So you were probably born in {bornYear()}
-      </p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <h1>Give Feedback</h1>
+      <Button onClick={handleGood} text="Good" />
+      <Button onClick={handleNeutral} text="Neutral" />
+      <Button onClick={handleBad} text="Bad" />
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
