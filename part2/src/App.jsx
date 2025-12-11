58d4f2085e23f25ffbabb8496043f4964df8dc4e
Aidan Stoaks <aidanstoaks05@gmail.com>
2025-12-10 21:11:59 -0800

    Exercise 2.1 complete

---
+++ part2/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = ({course}) => <h1>{course.name}</h1>
+
+const Content = ({course}) => (
+  <div>
+    <Part part={course.parts[0]} />
+    <Part part={course.parts[1]} />
+    <Part part={course.parts[2]} />
+  </div>
+)
+
+const Part = ({part}) => (
+  <p>
+    {part.name}: {part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises: {props.total}</p>
+
+const Course = ({ course }) => {
+  return(
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  )
+}
+  
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
+
+export default App
